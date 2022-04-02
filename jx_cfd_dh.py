0a8e15aa7815f0a050f5df7c7368b1ee8fe24038
windyday <63159472+CoverUp137@users.noreply.github.com>
2022-04-02 22:09:02 +0800

    Update jx_cfd_dh.py

--- jx_cfd_dh.py
+++ jx_cfd_dh.py
@@ -16,7 +16,7 @@ cfd_start_time = -0.15
 cfd_offset_time = 0.01
 
 # 基础配置勿动
-cfd_url = "https://m.jingxi.com/jxbfd/user/ExchangePrize?strZone=jxbfd&bizCode=jxbfd&source=jxbfd&dwEnv=7&_cfd_t=1648806375112&ptag=7155.9.47&dwType=3&dwLvl=6&ddwPaperMoney=100000&strPoolName=jxcfd2_exchange_hb_202204&strPgtimestamp=1648806374990&strPhoneID=8711198dabbe276e&strPgUUNum=5fa0a2ff1e591a30fc3225b0b8f6ef94&_stk=_cfd_t%2CbizCode%2CddwPaperMoney%2CdwEnv%2CdwLvl%2CdwType%2Cptag%2Csource%2CstrPgUUNum%2CstrPgtimestamp%2CstrPhoneID%2CstrPoolName%2CstrZone&_ste=1&h5st=20220401174615112%3B2859985974120068%3B92a36%3Btk02wd2bf1c8c18nZgkgc3TmNSxH3zTOytTUghKcclP%2But3cieEFGmvUX6h148mpz8mMLege3VmOOjwCpt%2BI4A%2BiNT2g%3B316b4a8712dc684ba9d4ccf975bf6afc2f823e715a1c021dfc122c9ef6092ca9%3B3.0%3B1648806375112&_=1648806375114&sceneval=2&g_login_type=1&callback=jsonpCBKLLL&g_ty=ls"
+cfd_url = "https://m.jingxi.com/jxbfd/user/ExchangePrize?strZone=jxbfd&bizCode=jxbfd&source=jxbfd&dwEnv=7&_cfd_t=1648778416388&ptag=7155.9.47&dwType=3&dwLvl=2&ddwPaperMoney=100000&strPoolName=jxcfd2_exchange_hb_202204&strPgtimestamp=1648778416345&strPhoneID=1ef0eafd7b25011a040a37ab925a5586ebfe3f35&strPgUUNum=1674e9a767125864a580ba003b8e655b&_stk=_cfd_t%2CbizCode%2CddwPaperMoney%2CdwEnv%2CdwLvl%2CdwType%2Cptag%2Csource%2CstrPgUUNum%2CstrPgtimestamp%2CstrPhoneID%2CstrPoolName%2CstrZone&_ste=1&h5st=20220401100016388%3B9785369927142102%3B92a36%3Btk02w5c951a1218nwt7hCp33UXI2Fjh60u3JfB%2BJ8MEWiMV%2Fibq4jLJPa3oZjNWJAHB1M35JEoTGURFBUSdW9vmWI3Y4%3B62cd48fa4161a161d28b0491ea99225665b121721a11208c5ffe19b0cf1cf4c7%3B3.0%3B1648778416388&_=1648778416389&sceneval=2&g_login_type=1&callback=jsonpCBKX&g_ty=ls"
 
 pattern_pin = re.compile(r'pt_pin=([\w\W]*?);')
 pattern_data = re.compile(r'\(([\w\W]*?)\)')
