5d5cae68aa4bfb842fe4e13661b84dd71676f794
windyday <63159472+CoverUp137@users.noreply.github.com>
2022-04-20 04:15:08 +0800

    Update jx_cfd_dh.py
    1

--- jx_cfd_dh.py
+++ jx_cfd_dh.py
@@ -85,9 +85,9 @@ def cfd_qq(def_start_time):
     re_list = pattern_data.search(res.text)
     # 进行json转换
     data = json.loads(re_list.group(1))
-    msg = data['sErrMsg']
+    msg = data['iRet']
     # 根据返回值判断
-    if data['story'] == 1:
+    if data['iRet'] == 0:
         # 抢到了
         msg = "可能抢到了"
         put_envs(u_cookie.get('_id'), u_cookie.get('name'), u_cookie.get('value'), msg)
