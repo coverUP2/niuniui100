a9b0268687587464fafc695387abbf2df5e92235
windyday <63159472+CoverUp137@users.noreply.github.com>
2022-04-20 04:22:30 +0800

    Update jx_cfd_dh.py
    1

--- jx_cfd_dh.py
+++ jx_cfd_dh.py
@@ -85,9 +85,9 @@ def cfd_qq(def_start_time):
     re_list = pattern_data.search(res.text)
     # 进行json转换
     data = json.loads(re_list.group(1))
-    msg = data['dwWaitTriTime']
+    msg = data['sErrMsg']
     # 根据返回值判断
-    if data['iRet'] == 0:
+    if data['dwtype'] == 3:
         # 抢到了
         msg = "可能抢到了"
         put_envs(u_cookie.get('_id'), u_cookie.get('name'), u_cookie.get('value'), msg)
